e3d0d6e6a81c511fb833738ab21efc2cc23daf33
ricardo.lopes.0133539 <ricardo.lopes.T0133539@edu.atec.pt>
2026-05-07 21:26:52 +0100

    Atualização do script e integração no HTML e inserção de parágrafo colaborativo

---
+++ script.js
@@ -0,0 +1,9 @@
+// Contribuição de Ricardo Lopes para o projeto colaborativo
+console.log("Script carregado com sucesso!");
+
+function boasVindas() {
+    alert("Bem-vindo ao nosso projeto Git Colaborativo!");
+}
+
+// Executa a função ao carregar a página
+boasVindas();
